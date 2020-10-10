25d67a9022909bd09a673daa2f78a70905c72d4e
Borislove <abirme@yandex.ru>
2020-10-10 04:47:30 +0300

    do while js

---
+++ dowhile.js
@@ -0,0 +1,10 @@
+var digit = 2;
+
+do {
+    console.log('ok!');
+digit += 1;
+} while (digit <= 10);
+
+console.log(digit + 'More than 10!')
+
+//11, добавит +1 и закончит, цикл с пост условием, выводит тело цикла 1 раз
